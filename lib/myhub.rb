7381b783a7aae0d62c26eda1b59d2a832dfed913
Harrison Andrews <harrisonandrews1986@gmail.com>
2015-10-22 08:58:12 -0400

    added html & css

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -11,6 +11,37 @@ module Myhub
 
     # Your code here ...
 
+    get "/" do
+      binding.pry
+      api = Github.new
+      #get stuff from github
+      stuff=api.get_issues
+      erb :index, locals: {issues: stuff}
+    end
+
+
+  post "/issue/reopen/:id" do
+    api = Github.new
+    api.reopen_issue(params["id"].to_i)
+    "Cool Cool Cool"
+  end
+
+
+   post "/issue/close/:id" do
+     api = Github.new
+     api.close_issue(params["id"].to_i)
+     "the issue is closed"
+end
+
     run! if app_file == $0
   end
 end
+
+# put "/issue/:id" do
+#   api = Github.new
+#   if params["state"] == "reopen"
+#     api.reopen_issue(params["id"].to_i)
+#   elsif params["state"] == "close"
+#     api.close_issue(params["id"].to_i)
+#   end
+# end
--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -1,8 +1,50 @@
+
 module Myhub
   class Github
     include HTTParty
     base_uri "https://api.github.com"
 
     # Your code here too!
+    def initialize
+      @headers = {
+          "Authorization"  => "token #{ENV["AUTH_TOKEN"]}",
+          "User-Agent"     => "HTTParty"
+      }
+    end
+
+    def get_issues
+      Github.get(
+          "org/TIY-ATL-ROR-2015-Sep/assignments/issues",
+      :headers => @headers,
+      :query => {state: "all"}
+      )
+
+    end
+
+
+    def reopen_issue(id)
+      Github.patch(
+                "/repos/TIY-ATL-ROR-2015-Sep/assignments/issues/#{id}",
+                :headers => @headers,
+                :body    => ({ :state => "open"}).to_json
+      )
+
+
+    end
+
+
+    def close_issue(id)
+      Github.patch(
+          "/repos/TIY-ATL-ROR-2015-Sep/assignments/issues/#{id}",
+          :headers => @headers,
+          :body    => ({ :state => "closed" }).to_json
+      )
+    end
+
+
+
+
+
+
   end
 end
